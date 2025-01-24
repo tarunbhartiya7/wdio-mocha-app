97378f25249991da04b10412bdcbbcaa2dc078f2
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2025-01-24 19:12:34 +0530

    add test to validate tables

--- test/specs/functionalScenarios.js
+++ test/specs/functionalScenarios.js
@@ -9,4 +9,16 @@ describe("functional testing", () => {
       "https://rahulshettyacademy.com/AutomationPractice/#top"
     );
   });
+
+  it.only("should validate web tables", async () => {
+    await browser.url("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+    await $("th:first-child").click()
+    await browser.pause(2000)
+    const list = await $$("tr td:first-child")
+    const veggies = await list.map(async item => await item.getText())
+    console.log("veggies: ", veggies)
+    const copy =  veggies.slice()
+    veggies.sort()
+    expect(veggies).toEqual(copy)
+  });
 });
